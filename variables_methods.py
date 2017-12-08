135b8e43d09e758e8b32a54fa209a56407677a03
Parth Chauhan <parthchauhan05@gmail.com>
2017-12-08 17:38:29 +0530

    Lec 10

--- variables_methods.py
+++ variables_methods.py
@@ -6,6 +6,18 @@ my_10_variable = 10
 string_variable = "hello"
 single_quote_variable = 'Strings can have single quotes'
 
-print(my_variable)
-print(string_variable)
-print("hello, World!")
+#print(my_variable)
+#print(string_variable)
+#print("hello, World!")
+
+##
+
+def my_print_method(my_parameter):
+    print(my_parameter)
+    
+#my_print_method('Hello, World!')
+
+def my_multiply_method(num1, num2):
+    return num1 * num2
+
+print(my_multiply_method(5,7))
