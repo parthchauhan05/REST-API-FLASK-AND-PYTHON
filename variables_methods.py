8f76ea582f0be89c2b8a17fed0424a1cb4548f7b
Parth Chauhan <parthchauhan05@gmail.com>
2017-12-08 16:52:55 +0530

    Lec 9

---
+++ variables_methods.py
@@ -0,0 +1,11 @@
+a = 5
+b = 10
+my_variable = 56
+my_10_variable = 10
+
+string_variable = "hello"
+single_quote_variable = 'Strings can have single quotes'
+
+print(my_variable)
+print(string_variable)
+print("hello, World!")
